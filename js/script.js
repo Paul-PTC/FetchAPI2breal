e039abb56b7e0b595bcb7e687061e5849a35601f
Mario <20210212@ricaldone.edu.sv>
2025-06-03 09:56:10 -0600

    Función para eliminar registros(funcional)

--- js/script.js
+++ js/script.js
@@ -1,111 +1,96 @@
-//URL de la API - Endpoint
+//URL de la api - EndPoint
 const API_URL = "https://retoolapi.dev/NPe0qm/expo";
-
+ 
 //Función para llamar a la API y traer el JSON
-async function ObtenerPersonas() {
-    
+async function ObtenerPersonas(){
     //Obtenemos la respuesta del servidor
-    const res = await fetch(API_URL);
-
+    const res = await fetch(API_URL); //Obtener datos de la API
+ 
     //Convertir la respuesta del servidor a formato JSON
-    const data = await res.json(); //Esto es un JSON
-
-    CrearTabla(data); //Enviamos el JSON a la función "CrearTabla".
+    const data = await res.json();
+ 
+    CrearTabla(data); //Enviamos al JSON a la fúncion "CrearTabla"
 }
-
+ 
 //Función que creará las filas de la tabla en base a los registros que vienen de la API
-
-function CrearTabla(datos){ //"Datos" representa al JSON que viene de la API
-
+function CrearTabla(datos){//Datos representa al JSON qeu viene  de la api
     //Se llama al "tbody" dentro de la tabla con id "tabla"
     const tabla = document.querySelector("#tabla tbody");
-
+ 
     //Para inyectar código HTML usamos "innerHTML"
-    tabla.innerHTML = ""; //Variamos el contenido de la tabla
-
+    tabla.innerHTML = ""; //Vacíamos el contenido de la tabla
+ 
     datos.forEach(persona => {
         tabla.innerHTML += `
-        <tr>
-            <td>${persona.id}</td>
-            <td>${persona.nombre}</td>
-            <td>${persona.apellido}</td>
-            <td>${persona.edad}</td> 
-            <td>${persona.correo}</td>
-            <td>
-                <button>Editar</button>
-                <button onClick="EliminarRegistro(${persona.id})">Eliminar</button>
-            </td>
-        </tr>
+            <tr>
+                <td>${persona.id}</td>
+                <td>${persona.nombre}</td>
+                <td>${persona.apellido}</td>
+                <td>${persona.edad}</td>
+                <td>${persona.correo}</td>
+                <td>
+                    <button>Editar</button>
+                    <button onClick="EliminarRegistro(${persona.id})">Eliminar</button>
+                </td>
+            </tr>
         `
     });
 }
-
+ 
 ObtenerPersonas();
-
-
-
-
+ 
 //Proceso para agregar un nuevo registro
-const modal = document.getElementById("modalAgregar"); //Cuadro de diálogo
-const btnAgregar = document.getElementById("btnAbrirModal"); //+ para abrir
-const btnCerrar = document.getElementById("btnCerrarModal"); //x para cerrar
-
-
+const modal = document.getElementById("modalAgregar");
+const btnAgregar = document.getElementById("btnAbrirModal");
+const btnCerrar = document.getElementById("btnCerrarModal");
+ 
 btnAgregar.addEventListener("click", ()=>{
-    modal.showModal(); //Abrir Modal
+    modal.showModal();
 });
-
+ 
 btnCerrar.addEventListener("click", ()=>{
     modal.close(); //Cerrar Modal
 });
-
-//Agregar nuevo integrante desde el formulario
+ 
+//Agregar nuevo integrante
 document.getElementById("frmAgregarIntegrante").addEventListener("submit", async e => {
-    e.preventDefault(); //"e" representa el evento Submit - Evita que el formulario se envie
-
-    //Capturamos los valores del formulario
+    e.preventDefault();
+ 
     const nombre = document.getElementById("nombre").value.trim();
     const apellido = document.getElementById("apellido").value.trim();
     const edad = document.getElementById("edad").value.trim();
     const correo = document.getElementById("email").value.trim();
-
-    //Validación básica
+ 
     if(!nombre || !apellido || !correo || !edad){
-        alert("Complete todos los campos");
-        return; //Evita que el código siga ejecutándose
+        alert("Complete todo los cambios")
+        return;
     }
-
-    //Llamar a la API para enviar el usuario
-    const respuesta = await fetch(API_URL,{
+ 
+    const respuesta = await fetch(API_URL, {
         method: "POST",
-        headers: {'Content-type':'application/json'},
-        body: JSON.stringify({nombre, apellido, edad, correo}),
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({nombre, apellido, edad, correo})
     });
-
+ 
     if(respuesta.ok){
         alert("El registro fue agregado correctamente");
-
-        //Limpiar el formulario
+ 
         document.getElementById("frmAgregarIntegrante").reset();
-
-        //Cerrar el formulario
+ 
         modal.close();
-
-        //Recargar la tabal
+ 
         ObtenerPersonas();
     }
     else{
-        alert("Hubo un error al agregar");
+        alert("Hubo un error al agregar")
     }
-
-
-}); //Fin del fortmulario 
-
-
+});
+ 
+ 
 //Para eliminar registros
-async function EliminarRegistro(id){ //Se pide ID para borrar
-    if(comfim("¿Estás seguro de desear borrar el registro?")){
-        await fetch(`${API_URL}/${id}`, {method:'DELETE'});
-    ObtenerPersonas();
-    }
+async function EliminarRegistro(id) { //Se pide ID para borrar
+        if(confirm("¿Estas seguro de que desea borrar el registro?")){
+            await fetch(`${API_URL}/${id}`, {method: 'DELETE'});
+            ObtenerPersonas(); //Para refrescar
+        }
 }
